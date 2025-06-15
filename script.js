de15a5b54df1ab226434dfd6ba63cf3f6b33a0e2
unknown <melodynk01@gmail.com>
2025-06-15 12:19:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  //create add task
+  function addTask() {
+    const taskText = taskInput.value.trim();
+    if (!taskText) {
+      alert("Enter a task");
+      return;
+    } else {
+      //task content
+      const li = document.createElement("li");
+      li.textContent = taskText;
+      //Remove button
+      const btn = document.createElement("button");
+      btn.textContent = "Remove";
+      btn.className = "remove-btn";
+      btn.addEventListener("click", () => {
+        li.remove();
+      });
+      li.appendChild(btn);
+      taskList.appendChild(li);
+      taskInput.value = "";
+    }
+}
+addButton.addEventListener('click', addTask);
+taskInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        addTask();
+    }
+})
+document.addEventListener('DOMContentLoaded', () => {
+    addTask();
+})
+  //create display task
+  //create remove task
+});
